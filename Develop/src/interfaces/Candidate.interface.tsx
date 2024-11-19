6a864e947dcbf20de349bea18fbb768e6c7d9987
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-11-19 14:40:18 -0500

    created the interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// done: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+    login: string;
+    avatar_url: string;
+    name: string;
+    location: string;
+    email?: string;
+    company?: string;
+    html_url: string;
+  }
+  
