366c85aaf971f414fb6873b063decf715fd9e4a4
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-11-19 22:04:31 -0500

    nav bar and link

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  // DONE: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <ul style={{ display: 'flex', padding: 0 }}>
+        <li style={{ marginRight: '20px' }}>
+          <Link
+            to="/"
+            style={{ color: 'white' }}>
+            Home
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="/SavedCandidates"
+            style={{ color: 'white' }}>
+            Potential Candidates
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
